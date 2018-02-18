a06e34baea3e2b23002954159e0c843a8239f76f
Akash Bengeri <35753656+Akashbengeri@users.noreply.github.com>
2018-02-18 20:13:52 +0530

    Create BigFactorial.py

---
+++ BigFactorial.py
@@ -0,0 +1,57 @@
+import sys
+# Python program to compute factorial
+# of big numbers
+
+def extraLongFactorials( n) :
+    res = [None]*500
+    # Initialize result
+    res[0] = 1
+    res_size = 1
+ 
+    # Apply simple factorial formula 
+    # n! = 1 * 2 * 3 * 4...*n
+    x = 2
+    while x <= n :
+        res_size = multiply(x, res, res_size)
+        x = x + 1
+     
+    #print ("Factorial of given number is")
+    i = res_size-1
+    while i >= 0 :
+        sys.stdout.write(str(res[i]))
+        sys.stdout.flush()
+        i = i - 1
+         
+ 
+# This function multiplies x with the number 
+# represented by res[]. res_size is size of res[] 
+# or number of digits in the number represented 
+# by res[]. This function uses simple school 
+# mathematics for multiplication. This function 
+# may value of res_size and returns the new value
+# of res_size
+def multiply(x, res,res_size) :
+     
+    carry = 0 # Initialize carry
+ 
+    # One by one multiply n with individual
+    # digits of res[]
+    i = 0
+    while i < res_size :
+        prod = res[i] *x + carry
+        res[i] = prod % 10; # Store last digit of 
+                            # 'prod' in res[]
+        carry = prod/10; # Put rest in carry
+        i = i + 1
+ 
+    # Put carry in res and increase result size
+    while (carry) :
+        res[res_size] = carry % 10
+        carry = carry / 10
+        res_size = res_size + 1
+         
+    return res_size
+
+if __name__ == "__main__":
+    n = int(raw_input().strip())
+    extraLongFactorials(n)
